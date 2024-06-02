c737ed30f3dd1494b934a8fa9b94f2d1d6449e60
Brieuc Dubois <git@bhasher.com>
2024-06-02 18:18:50 +0200

    rofi: alnum sorting

--- default.nix
+++ default.nix
@@ -18,4 +18,5 @@
 
   aw-awatcher = pkgs.callPackage ./pkgs/aw-awatcher/package.nix { };
   hyprshot = pkgs.callPackage ./pkgs/hyprshot/package.nix { };
+  rofi-wayland = pkgs.callPackage ./pkgs/rofi/wayland.nix { };
 }
