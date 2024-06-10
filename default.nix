6945c359220d941de3729704531c24502a934659
Brieuc Dubois <git@bhasher.com>
2024-06-10 19:44:15 +0200

    Revert "Simplify rofi"
    This reverts commit 073593d0c681687f9b516cf11f32851d802d5fde.

--- default.nix
+++ default.nix
@@ -17,5 +17,5 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   aw-awatcher = pkgs.callPackage ./pkgs/aw-awatcher/package.nix { };
-  rofi = pkgs.callPackage ./pkgs/rofi { };
+  rofi-unwrapped = pkgs.callPackage ./pkgs/rofi { };
 }
